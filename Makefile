12f1861b52dc71946fc35612341dddebe84bb4a9
z1rtaee <piresbeatrizwrk@gmail.com>
2025-01-14 17:44:41 +0000

    Libft + Makefile + .h

---
+++ Makefile
@@ -0,0 +1,39 @@
+###COLOURS###
+PINK 	= \e[1;35m
+PURPLE 	= \e[0;95m
+WHITE   = \e[1;37m
+DEFAULT = \e[0m
+
+###VARIABLES###
+NAME = push_swap
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g
+SRC_DIR = src/
+LIBFT_DIR = Libft/
+INCLUDE = ./libft/libft.a
+
+SRCS = $(SRC_DIR)
+OBJS = $(SRCS: .c=.o)
+
+###RULES###
+%.o: %.c
+	$(CC) $(CFLAGS) -c $(<) -o $(@)
+
+$(NAME): $(OBJS)
+	@echo "$(PINK)Bringing $(PURPLE)$(NAME)$(PINK) to life!$(DEFAULT)"
+	@make -C $(LIBFT_DIR)
+	@echo "$(PINK)Creating $(PURPLE)$(NAME)'s$(PINK) executable$(DEFAULT)"
+	@$(CC) $(CFLAGS) $(OBJS) -o $(NAME) $(INCLUDE)
+	@echo "$(PURPLE)$(NAME) was born!"
+
+all: $(NAME)
+
+clean:
+	@rm -rf $(OBJS)
+
+fclean: clean
+	@rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ inc/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zirtaee <zirtaee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 16:21:40 by zirtaee           #+#    #+#             */
+/*   Updated: 2025/01/14 17:24:16 by zirtaee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../Libft/libft.h"
+
+typedef struct s_list
+{
+    struct s_list   *prev;
+    void            *content;
+    struct s_list   *next;
+}                   t_list;
+
+
+#endif
